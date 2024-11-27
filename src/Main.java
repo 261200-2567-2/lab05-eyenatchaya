9376f5287bf7874b642c30dd4acc18b9fe4c1510
ASUS TUF F15 <eyenatchaya@gmail.com>
2024-11-27 19:02:46 +0700

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+// Lab05 class for testing
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        // Create characters
+        Character warrior = new Warrior("Baldor", 10);
+        Character mage = new Mage("Merlin", 12);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Create accessories
+        Accessory strengthRing = new Ring("Ring of Strength", 5, 0);
+        Accessory intelligenceRing = new Ring("Ring of Intelligence", 0, 5);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Equip accessories
+        warrior.equipAccessory(strengthRing);
+        mage.equipAccessory(intelligenceRing);
+
+        // Display characters' stats
+        System.out.println(warrior);
+        System.out.println(mage);
+
+        // Use abilities
+        System.out.println(warrior.useAbility("Power Slash"));
+        System.out.println(mage.useAbility("Fireball"));
+
+        // Unequip accessories
+        warrior.unequipAccessory(strengthRing);
+        mage.unequipAccessory(intelligenceRing);
+
+        // Display stats after unequipping
+        System.out.println("After unequipping:");
+        System.out.println(warrior);
+        System.out.println(mage);
     }
 }
